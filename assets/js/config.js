ee1818594c323a17e194a38d8f79679cc891023d
Buney <dpkinser93@gmail.com>
2021-12-03 20:40:54 -0700

    removed json files and replaced with config

---
+++ assets/js/config.js
@@ -0,0 +1,20 @@
+const config = {
+
+    names: [
+        "Daniel Kinser",
+        "Gary Kinser",
+        "Savannah Wade"
+    ],
+
+    gifts: [
+        {
+            "title": "Test Gift",
+            "image": "2.gif"
+        },
+        {
+            "title": "Another Test Gift",
+            "image": "2.gif"
+        }
+    ]
+
+}
---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const $gameBoard = $("#game-board");
+const names = config.names;
+const gifts = config.gifts;
+
+console.log(names, gifts);
