da2f7105a90e2512f89b9d4595262485b592e548
Buney <dpkinser93@gmail.com>
2021-12-09 17:42:13 -0700

    added gifts to config

--- assets/js/config.js
+++ assets/js/config.js
@@ -8,17 +8,149 @@ const config = {
 
     gifts: [
         {
-            "title": "Test Gift",
-            "image": "2.gif"
+            'image': 'Amazon',
+            'title':'Amazon.png',
         },
         {
-            "title": "Another Test Gift",
-            "image": "2.gif"
+            'image': 'Apple',
+            'title':'Apple.png',
         },
         {
-            "title": "Test testerino",
-            "image": "2.gif"
-        }
-    ]
+            'image': 'BarnesNoble',
+            'title':'BarnesNoble.png',
+        },
+        {
+            'image': 'BathBodyWorks',
+            'title':'BathBodyWorks.png',
+        },
+        {
+            'image': 'BJs',
+            'title':'BJs.png',
+        },
+        {
+            'image': 'Cabelas',
+            'title':'Cabelas.png',
+        },
+        {
+            'image': 'Chili\'s',
+            'title': 'Chili\'s.png',
+        },
+        {
+            'image': 'Chipotle',
+            'title':'Chipotle.png',
+        },
+        {
+            'image': 'Coldstone',
+            'title':'Coldstone.png',
+        },
+        {
+            'image': 'Doordash',
+            'title':'Doordash.png',
+        },
+        {
+            'image': 'EddieBauer',
+            'title':'EddieBauer.png',
+        },
+        {
+            'image': 'Grubhub',
+            'title':'Grubhub.png',
+        },
+        {
+            'image': 'HomeDepot',
+            'title':'HomeDepot.png',
+        },
+        {
+            'image': 'LazyDog',
+            'title':'LazyDog.png',
+        },
+        {
+            'image': 'LLBean',
+            'title':'LLBean.png',
+        },
+        {
+            'image': 'Lowes',
+            'title':'Lowes.png',
+        },
+        {
+            'image': 'OntheBorder',
+            'title':'OntheBorder.png',
+        },
+        {
+            'image': 'PaneraBread',
+            'title':'PaneraBread.png',
+        },
+        {
+            'image': 'RedLobster',
+            'title':'RedLobster.png',
+        },
+        {
+            'image': 'RedRobin',
+            'title':'RedRobin.png',
+        },
+        {
+            'image': 'REI',
+            'title':'REI.png',
+        },
+        {
+            'image': 'RockBottom',
+            'title':'RockBottom.png',
+        },
+        {
+            'image': 'Saltgrass',
+            'title':'Saltgrass.png',
+        },
+        {
+            'image': 'Starbucks',
+            'title':'Starbucks.png',
+        },
+        {
+            'image': 'TexasRoadhouse',
+            'title':'TexasRoadhouse.png',
+        },
+        {
+            'image': 'TIComputerBackpack',
+            'title':'TIComputerBackpack.png',
+        },
+        {
+            'image': 'TIDuffle',
+            'title':'TIDuffle.png',
+        },
+        {
+            'image': 'TIHoodie',
+            'title':'TIHoodie.png',
+        },
+        {
+            'image': 'TILaptopBag',
+            'title':'TILaptopBag.png',
+        },
+        {
+            'image': 'TINikeCap',
+            'title':'TINikeCap.png',
+        },
+        {
+            'image': 'TItumbler',
+            'title':'TItumbler.png',
+        },
+        {
+            'image': 'TIVintageT',
+            'title':'TIVintageT.png',
+        },
+        {
+            'image': 'Ulta',
+            'title':'Ulta.png',
+        },
+        {
+            'image': 'USBcameramic',
+            'title':'USBcameramic.png',
+        },
+        {
+            'image': 'VictoriasSecret',
+            'title':'VictoriasSecret.png',
+        },
+        {
+            'image': 'Walmart',
+            'title':'Walmart.png',
+        },
+    ],
 
 }
